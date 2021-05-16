02c743c65195d9a0ffd879d787d1afbfc5825b2e
Efen Stakes <efenstakes101@gmail.com>
2021-05-17 02:39:13 +0300

    add text styles

---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+// title 1
+TextStyle title1 = GoogleFonts.karla(
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+
+
+// title 2
+TextStyle title2 = GoogleFonts.karla(
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+
+
+// title 3
+TextStyle title3 = GoogleFonts.karla(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+
+
+// text
+TextStyle text1 = GoogleFonts.lato(
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+
+
+// text
+TextStyle text2 = GoogleFonts.lato(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+
+// text
+TextStyle text3 = GoogleFonts.lato(
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+);
+
+
+// text
+TextStyle text4 = GoogleFonts.lato(
+  fontSize: 12,
+  fontWeight: FontWeight.bold,
+);
