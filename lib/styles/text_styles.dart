1ccae08175b0414874e897228cfdaeb6569e6244
Efen Stakes <efenstakes101@gmail.com>
2021-05-17 16:27:07 +0300

    add more text styles

--- lib/styles/text_styles.dart
+++ lib/styles/text_styles.dart
@@ -2,51 +2,79 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 
+// big title 1
+TextStyle btitle1 = GoogleFonts.karla(
+  fontSize: 40,
+  fontWeight: FontWeight.bold,
+);
+
+// big title 2
+TextStyle btitle2 = GoogleFonts.karla(
+  fontSize: 36,
+  fontWeight: FontWeight.bold,
+);
+
+// big title 3
+TextStyle btitle3 = GoogleFonts.karla(
+  fontSize: 32,
+  fontWeight: FontWeight.bold,
+);
+
 // title 1
 TextStyle title1 = GoogleFonts.karla(
-  fontSize: 20,
+  fontSize: 24,
   fontWeight: FontWeight.bold,
 );
 
 
 // title 2
 TextStyle title2 = GoogleFonts.karla(
-  fontSize: 18,
+  fontSize: 22,
   fontWeight: FontWeight.bold,
 );
 
 
 // title 3
 TextStyle title3 = GoogleFonts.karla(
-  fontSize: 16,
+  fontSize: 20,
   fontWeight: FontWeight.bold,
 );
 
 
 
+// title 3
+TextStyle title4 = GoogleFonts.karla(
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+
+
+// title 3
+TextStyle title5 = GoogleFonts.karla(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+
 // text
 TextStyle text1 = GoogleFonts.lato(
   fontSize: 18,
-  fontWeight: FontWeight.bold,
 );
 
 
 // text
 TextStyle text2 = GoogleFonts.lato(
   fontSize: 16,
-  fontWeight: FontWeight.bold,
 );
 
 
 // text
 TextStyle text3 = GoogleFonts.lato(
   fontSize: 14,
-  fontWeight: FontWeight.bold,
 );
 
 
 // text
 TextStyle text4 = GoogleFonts.lato(
   fontSize: 12,
-  fontWeight: FontWeight.bold,
 );
