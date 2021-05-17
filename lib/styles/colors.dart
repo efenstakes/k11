9ecd43c2fb852f2bcc3abba590fefa6bc73b662e
Efen Stakes <efenstakes101@gmail.com>
2021-05-17 16:26:55 +0300

    add app colors

---
+++ lib/styles/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color primary = Color(0x73B2D9);
+Color success = Color(0x63BF21);
+Color successDark = Color(0x488C16);
+Color error = Color(0xFFC409);
+Color errorAlt = Colors.yellow[600];
